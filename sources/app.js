10f3a180d77cb1908fb6492b20232e764f7e3ab6
Fernando Brum <fernandobalcantara@gmail.com>
2017-07-01 23:35:42 -0300

    inclusao da pagina html para os logs

--- sources/app.js
+++ sources/app.js
@@ -5,6 +5,7 @@ var router = require('./router.js')
 var bodyParser = require('body-parser')
 var expressValidator = require('express-validator')
 //
+
 var cV = require('./validate/validate.controller.js').customValidators
 
 //
@@ -16,6 +17,7 @@ server
 	.use(bodyParser.json())
 	.use(expressValidator({customValidators: cV}))
 	.use('/api/doc', express.static('doc'))
+	.use('/api/log', express.static('log_files'))
 	.use('/api', router)
 
 mongoose.Promise = bluebird
@@ -23,8 +25,13 @@ mongoose.Promise = bluebird
 const mongoconect = 'mongodb://seriesadm:brum@ds129018.mlab.com:29018/seriesdb'
 
 mongoose.connect(mongoconect, err => {
+
   if (err) {
+
     return console.log('error on connect db')
+    
   }
+
   server.listen(port, () => console.log(`localhost:${port}`))
+
 })
